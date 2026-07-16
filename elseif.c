4b7faba2daed2264073e9f72edbfbf9a5cb16f68
Maanisha29 <maanisha.govindasamy@gmail.com>
2026-07-16 20:54:39 +0530

    Modified elseif.c file

--- elseif.c
+++ elseif.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c", &n);
+    if(n>='a' && n<='z')
+    printf("Lowercase");
+    else if(n>='A' && n<='Z')
+    printf("Uppercase");
+    else
+    printf("Number")
+}
