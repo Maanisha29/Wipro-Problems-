0aac7033041177f8592ce54ebf542e6fd19274cb
Maanisha29 <maanisha.govindasamy@gmail.com>
2026-07-16 20:57:30 +0530

    Modified switchcase.c

---
+++ switchcase.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c", &a);
+    switch(a)
+    {
+        case 'A'.....'Z':printf("Uppercase");
+        break;
+        case 'a'.....'z':printf("Lowercase");
+        break;
+        case '0'.....'9':printf("Number");
+        break;
+    }
+
+}
